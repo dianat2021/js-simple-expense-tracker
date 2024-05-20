ceed0f7718d342ba2e44495418d710e41206b4cc
= <=>
2024-05-20 17:05:43 +0200

    implement the delete feature

---
+++ src/js/deleteExpense.js
@@ -0,0 +1,12 @@
+import renderExpenses from "./renderExpenses.js";
+const deleteExpense = (expenseList, expense) => {
+  const deleteIndex = expenseList.findIndex(
+    (element) => element.id === expense.id
+  );
+
+  expenseList.splice(deleteIndex, 1);
+  localStorage.setItem("expense-array", JSON.stringify(expenseList));
+  renderExpenses(expenseList);
+};
+
+export default deleteExpense;
